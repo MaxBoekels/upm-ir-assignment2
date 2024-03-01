905e80af2cd728bebcc6e75836505fbc32ecb820
MaxBoekels <maximilian.boekels@rwth-aachen.de>
2024-03-01 21:09:29 +0100

    Adjusted function parameters to Series, unified input data to normalized string, started implementation of train_model() function

--- queryranking.py
+++ queryranking.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import re
+
 from lightgbm import LGBMRanker
 from IPython.display import display
 
@@ -40,6 +42,13 @@ class QueryRanking:
         self.data = {}
         for query in self.queries:
             self.data[query] = dataset[query].copy()
+
+            # create new attribute content that contains the content from all columns as one string
+            self.data[query]['content'] = self.data[query].apply(lambda row: ' '.join(row), axis=1)
+            self.data[query]['content'] = self.data[query]['content'].apply(self.clean_string)
+
+            
+            # create new attribute rank that describes the rank that each document has for the specific query
             self.data[query]['rank'] = len(self.data[query].index) - self.data[query].index
 
     """
@@ -47,18 +56,33 @@ class QueryRanking:
     Train model based on data provided
     """
     def train_model(self):
-        pass
+        model_data = pd.DataFrame(columns=['qaf', 'qrf', 'daf', 'drf', 'idf', 'rfad', 'rank'])
+        for query in self.queries:
+            data = self.data[query]
+
+            # generate clue data
+            clue_data = self.generate_clue_data(data['content'], query)
+            # copy ranks from original data
+            clue_data['rank'] = data['rank']
+
+            # append query specific data to overall dataframe 
+            model_data = pd.concat([model_data, clue_data], ignore_index=True)
+
+        # normalize values
+        # initialize and train model
+        # save model to self.model
+
     
     """
     TODO
     Predict by ranking an input dataframe, converting it to clue data and predicting using the model
     """
-    def predict_ranking(self, rank_data: pd.DataFrame, query: str) -> pd.DataFrame:
+    def predict_ranking(self, rank_data: pd.Series, query: str) -> pd.DataFrame:
         if self.model == None:
             self.train_model()
         
         result = rank_data.copy()
-        # generate clue data to apply the model
+        # generate clue data for testing data to apply the model to
         data = self.generate_clue_data(rank_data, query)
 
         # predict ranks with model
@@ -69,18 +93,21 @@ class QueryRanking:
     """
     Generate clue data for an input dataframe
     """
-    def generate_clue_data(self, rank_data: pd.DataFrame, query: str) -> pd.DataFrame:
+    def generate_clue_data(self, rank_data: pd.Series, query: str) -> pd.DataFrame:
         # initialize dataframe
         data_columns = ['qaf', 'qrf', 'daf', 'drf', 'idf', 'rfad']
         data = pd.DataFrame(columns=data_columns)
 
+        # clean query from special characters
+        clean_query = self.clean_string(query)
+
         # generate column data
-        data['qaf'] = self.query_absolute_frequency(rank_data, query)
-        data['qrf'] = self.query_relative_frequency(rank_data, query)
-        data['daf'] = self.document_absolute_frequency(rank_data, query)
-        data['drf'] = self.document_relative_frequency(rank_data, query)
-        data['idf'] = self.inverse_document_frequency(rank_data, query)
-        data['rfad'] = self.relative_frequency_all_documents(rank_data, query)
+        data['qaf'] = self.query_absolute_frequency(rank_data, clean_query)
+        data['qrf'] = self.query_relative_frequency(rank_data, clean_query)
+        data['daf'] = self.document_absolute_frequency(rank_data, clean_query)
+        data['drf'] = self.document_relative_frequency(rank_data, clean_query)
+        data['idf'] = self.inverse_document_frequency(rank_data, clean_query)
+        data['rfad'] = self.relative_frequency_all_documents(rank_data, clean_query)
         return data
     
     """
@@ -93,37 +120,37 @@ class QueryRanking:
     """
     TODO
     """
-    def query_absolute_frequency(self, rank_data: pd.DataFrame, query: str) -> pd.Series:
+    def query_absolute_frequency(self, rank_data: pd.Series, query: str) -> pd.Series:
         pass
 
     """
     TODO
     """
-    def query_relative_frequency(self, rank_data: pd.DataFrame, query: str) -> pd.Series:
+    def query_relative_frequency(self, rank_data: pd.Series, query: str) -> pd.Series:
         pass
 
     """
     TODO
     """
-    def document_absolute_frequency(self, rank_data: pd.DataFrame, query: str) -> pd.Series:
+    def document_absolute_frequency(self, rank_data: pd.Series, query: str) -> pd.Series:
         pass
 
     """
     TODO
     """
-    def document_relative_frequency(self, rank_data: pd.DataFrame, query: str) -> pd.Series:
+    def document_relative_frequency(self, rank_data: pd.Series, query: str) -> pd.Series:
         pass
     
     """
     TODO
     """
-    def inverse_document_frequency(self, rank_data: pd.DataFrame, query: str) -> pd.Series:
+    def inverse_document_frequency(self, rank_data: pd.Series, query: str) -> pd.Series:
         pass
     
     """
     TODO
     """
-    def relative_frequency_all_documents(self, rank_data: pd.DataFrame, query: str) -> pd.Series:
+    def relative_frequency_all_documents(self, rank_data: pd.Series, query: str) -> pd.Series:
         pass
     
     """
@@ -133,7 +160,17 @@ class QueryRanking:
     def standardize_values(self, column: pd.Series) -> pd.Series:
         pass
     
-
+    """
+    Helper function to clean the string from double spaced, leading and trailing spaces and special characters
+    """
+    def clean_string(self, input_string: str) -> str:
+        # Remove special characters except '#'
+        cleaned_string = re.sub(r'[^\w\s#]', ' ', input_string)
+        # Collapse consecutive spaces into a single space
+        cleaned_string = re.sub(r'\s+', ' ', cleaned_string)
+        # Strip leading and trailing spaces
+        cleaned_string = cleaned_string.strip()
+        return cleaned_string
 
 
 
@@ -147,7 +184,7 @@ Main function to create QueryRanking object and generate model
 def main():
     queries = ['glucose in blood', 'bilirubin in plasma', 'White blood cells count']
     qr = QueryRanking(queries)
-
+    """
     # change when needed
     column_names = ['loinc_num', 'loinc_common_name', 'component', 'system', 'property']
     data = pd.DataFrame(columns=column_names)
@@ -156,6 +193,9 @@ def main():
 
     results = qr.predict_ranking(data, query)
     display(results)
+
+    # evaluate accuracy using the function evaluate_performance()
+    """
 
 
 if __name__ == "__main__":
